b208327100fe3b99dd586e7f1df3b7579638418a
Hana <nyxsdev@gmail.com>
2019-09-30 11:51:26 +0900

    fix Error (add theLargest func)

--- 1. sort/1. Selection Sort.py
+++ 1. sort/1. Selection Sort.py
@@ -1,12 +1,15 @@
+def theLargest(arr, i):
+    select = 0
+    for j in range(1, i + 1):
+        if arr[j] > arr[select]:
+            select = j
+    return select
+
 def selection_sort(arr):
     for i in range(len(arr) - 1, 1, -1):
-        select = 0
-        for j in range(1, i):
-            if arr[j] > arr[select]:
-                select = j
+        select = theLargest(arr, i)
         arr[i], arr[select] = arr[select], arr[i]
     return arr
-
 
 testcase = [3,31,48,73,8,11,20,29,65,15]
 print("Selection Sort", selection_sort(testcase))
