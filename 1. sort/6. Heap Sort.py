697e56c6716bc9f9b3907d913f98c570e50a306d
Hana <decomposes@naver.com>
2019-10-02 13:00:49 +0900

    del return val

--- 1. sort/6. Heap Sort.py
+++ 1. sort/6. Heap Sort.py
@@ -15,19 +15,18 @@ def heapify(A, k ,n):
 
     if A[smaller] < A[k]:
         A[k], A[smaller] = A[smaller], A[k]
-        heapify(A,smaller,n)
+        heapify(A, smaller, n)
 
 def buildHeap(A, n):
-    for i in range(int(n/2),-1, -1):
+    for i in range(int(n/2), -1, -1):
         heapify(A, i, n)
 
 def heap_sort(A):
     n = len(testcase) - 1
     buildHeap(A,n)
     for i in range(n, 0, -1):
-        A[0] , A[i] = A[i], A[0]
-        heapify(A,0,i-1)
-    return A
+        A[0], A[i] = A[i], A[0]
+        heapify(A, 0, i-1)
 
 testcase = [3,31,48,73,8,11,20,29,65,15]
 heap_sort(testcase)
