fad9c78be54f1174e6df19a0c514f2623dd6f7fb
Hana <decomposes@naver.com>
2019-10-02 12:46:49 +0900

    add base by sort

--- 1. sort/7. Radix Sort.py
+++ 1. sort/7. Radix Sort.py
@@ -1,24 +1,30 @@
-def getdigit(num, k):
-    return num % pow(10,k) // pow(10,k-1)
+from math import log
 
-def stable_sort(a, n, k):
-    b = [0] * 10
+def getdigit(num, k, base):
+    return num % pow(base,k) // pow(base,k-1)
+
+def stable_sort(a, k, base):
+    n = len(a)
+    b = [0] * base
     c = [0] * n
     for i in range(n):
-        b[getdigit(a[i], k)] += 1
+        b[getdigit(a[i], k, base)] += 1
         
-    for i in range(1, 10):
+    for i in range(1, base):
         b[i] += b[i-1]
 
     for i in range(n - 1, -1 , -1):
-        c[b[getdigit(a[i], k)] - 1] = a[i]
-        b[getdigit(a[i], k)] -= 1
-    return c
+        c[b[getdigit(a[i], k, base)] - 1] = a[i]
+        b[getdigit(a[i], k, base)] -= 1
 
-def radix_sort(a, n, k):
+    for i in range(n):
+        a[i] = c[i]
+
+def radix_sort(a, base):
+    k = int(log(max(a), base) + 1)
     for i in range(1, k + 1):
-        a = stable_sort(a, n, i)
-    return a
+        stable_sort(a, i, base)
 
 testcase = [123,2154,222,4,223,1560,1061,2150]
-print("radix Sort", radix_sort(testcase, len(testcase), 4))
+radix_sort(testcase, 10)
+print("radix Sort", testcase)
