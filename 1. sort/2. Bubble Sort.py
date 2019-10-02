bc4b017915a366f77f312d2e2f7762ae5e0a39c2
Hana <decomposes@naver.com>
2019-10-02 12:53:04 +0900

    change return

--- 1. sort/2. Bubble Sort.py
+++ 1. sort/2. Bubble Sort.py
@@ -3,7 +3,7 @@ def bubble_sort(arr):
         for j in range(0, i):
             if arr[j] > arr[j + 1]:
                arr[j], arr[j+1] = arr[j+1], arr[j]
-    return arr
 
 testcase = [3,31,48,73,8,11,20,29,65,15]
-print("Bubble Sort", bubble_sort(testcase))
+bubble_sort(testcase)
+print("Bubble Sort", testcase)
