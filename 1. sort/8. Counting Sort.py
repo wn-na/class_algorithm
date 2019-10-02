782a8e9fe864927d9a2e040dfab273d7eec9f456
Hana <decomposes@naver.com>
2019-10-02 12:49:19 +0900

    change val

--- 1. sort/8. Counting Sort.py
+++ 1. sort/8. Counting Sort.py
@@ -1,15 +1,22 @@
-def counting_sort(a, n, k):
+def counting_sort(a):
+    k = max(a)
+    n = len(a)
     b = [0] * n
     c = [0] * n
+    
     for i in range(n):
         c[a[i]] += 1
+        
     for i in range(1, k+1):
         c[i] += c[i-1]
+        
     for i in range(n - 1, -1, -1):
         b[c[a[i]] - 1] = a[i]
         c[a[i]] -= 1
-    return b
-
+        
+    for i in range(n):
+        a[i] = b[i]
 
 testcase = [1,1,1,3,5,2,2,2,6,4,2,1]
-print("Counting Sort", counting_sort(testcase, len(testcase), 6))
+counting_sort(testcase)
+print("Counting Sort", testcase)
